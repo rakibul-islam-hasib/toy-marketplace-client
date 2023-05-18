c594845903ee61cb11fedbe14f7282a6f8c183ab
Hasib Hossain <boraborhasib@gmail.com>
2023-05-18 22:26:37 +0600

    Add toy page updated

--- src/components/actions/AddToy.jsx
+++ src/components/actions/AddToy.jsx
@@ -1,33 +1,59 @@
 import { Box, FormControl, InputLabel, MenuItem, Select, TextField } from '@mui/material';
 
-import React, { useContext, useEffect, useRef } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
+import Swal from 'sweetalert2';
 import { AuthContext } from '../../provider/AuthProvider';
+import { motion } from 'framer-motion';
 
 const AddToy = () => {
 
     const nameRef = useRef(null);
-    const emailRef = useRef(null);
-    const [age, setAge] = React.useState('');
     const { user } = useContext(AuthContext);
+    const handelFromSubmit = e => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+        const data = Object.fromEntries(formData);
+        fetch(`http://localhost:5000/api/add-toy`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if (data.insertedId) {
+                    e.target.reset();
+                    Swal.fire(
+                        'Good job!',
+                        'Your toys successfully added to our database!',
+                        'success'
+                    )
+                }
+            })
+        console.log(data);
 
-    const handleChange = (event) => {
-        setAge(event.target.value);
-    };
+    }
+
+
     useEffect(() => {
         document.title = 'Zooming Wheels | Add Toy';
     }, [])
     return (
-        <div>
+        <div className='mb-14'>
             <div className="mb-12">
                 <h1 className='text-5xl mt-11 mb-5 hover:text-secondary duration-300 cursor-pointer text-center'>Sell your toys</h1>
                 <p className='text-base text-center text-secondary hover:text-primary cursor-pointer duration-300'>Please fill this form to sell your toys</p>
             </div>
-            <form className='w-[80%] mx-auto' action="">
+            <form onSubmit={handelFromSubmit} className='w-[80%] mx-auto'>
                 <h1 className='text-center my-4'>Seller Info</h1>
                 <div className="flex gap-7">
                     <TextField
                         id="outlined-basic"
                         label="Name"
+                        required
+                        name='name'
                         variant="outlined"
                         className='w-full mb-5'
                         inputRef={nameRef}
@@ -36,9 +62,10 @@ const AddToy = () => {
                     <TextField
                         id="outlined-basic"
                         label="Email"
+                        name='email'
                         variant="outlined"
                         className='w-full mb-5'
-                        // inputRef={nameRef}
+                        required
                         defaultValue={user?.email ? user.email : ''}
                     />
                 </div>
@@ -47,25 +74,114 @@ const AddToy = () => {
                     <TextField
                         id="outlined-basic"
                         label="Toy Name"
+                        name='toyName'
+                        required
                         variant="outlined"
                         className='w-full mb-5'
                         inputRef={nameRef}
                     />
-                    <Box sx={{ minWidth: 288 }}>
-                        <InputLabel id="demo-simple-select-label">Age</InputLabel>
-                        <Select
-                            labelId="demo-simple-select-label"
-                            id="demo-simple-select"
-                            value={age}
-                            label="Age"
-                            onChange={handleChange}
-                        >
-                            <MenuItem value={10}>Ten</MenuItem>
-                            <MenuItem value={20}>Twenty</MenuItem>
-                            <MenuItem value={30}>Thirty</MenuItem>
-                        </Select>
+                    <TextField
+                        id="outlined-basic"
+                        label="Photo URL"
+                        name='photo'
+                        required
+                        variant="outlined"
+                        className='w-full mb-5'
+                    />
+
+                </div>
+                <h1 className='text-center my-4'>Category And Sub Category</h1>
+                <div className="flex gap-7">
+                    <Box sx={{ width: '100%' }}>
+                        <FormControl fullWidth>
+                            <InputLabel id="demo-simple-select-label">Category</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                // value={category}
+                                required
+                                label="Category"
+                                name='category'
+                                defaultValue={`10`}
+                                // onChange={handleSubCategoryChange}
+                            >
+                                <MenuItem value={`10`}>Ten</MenuItem>
+                                <MenuItem value={`20`}>Twenty</MenuItem>
+                                <MenuItem value={`30`}>Thirty</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </Box>
+                    <Box sx={{ width: '100%' }}>
+                        <FormControl fullWidth>
+                            <InputLabel id="demo-simple-select-label">Sub Category</InputLabel>
+                            <Select
+                                labelId="demo-simple-select-label"
+                                id="demo-simple-select"
+                                // value={subCategory}
+                                required
+                                defaultValue={`car`}
+                                label="Sub Category"
+                                name='subCategory'
+                                // onChange={handleCategoryChange}
+                            >
+                                <MenuItem value='car'>Car</MenuItem>
+                                <MenuItem value='bus'>Bus</MenuItem>
+                                <MenuItem value='truck'>Truck</MenuItem>
+                            </Select>
+                        </FormControl>
                     </Box>
                 </div>
+                <h1 className='text-center my-4'>Price & Ratings </h1>
+                <div className="flex gap-7">
+                    <TextField
+                        id="outlined-basic"
+                        label="Price"
+                        required
+                        name='price'
+                        variant="outlined"
+                        className='w-full mb-5'
+                    />
+                    <TextField
+                        id="outlined-basic"
+                        label="Ratings"
+                        required
+                        name='ratings'
+                        variant="outlined"
+                        className='w-full mb-5'
+                    />
+                </div>
+                <div className="">
+                    <h1 className='text-center my-4'>Quantity & Photo</h1>
+                    <div className="flex gap-7">
+                        <TextField
+                            id="outlined-basic"
+                            name='quantity'
+                            required
+                            label="Quantity"
+                            variant="outlined"
+                            className='w-full mb-5'
+                        />
+                    </div>
+                    <legend>Description</legend>
+                    <textarea
+                        className='w-full resize-none border-2 outline-none rounded-md p-3   mb-5'
+                        name="description"
+                        required
+                        placeholder='Description'
+                        id="description"
+                        cols="30"
+                        rows="8"
+                    ></textarea>
+
+                </div>
+                <motion.button
+                    whileHover={{ scale: 1 }}
+                    whileTap={{ scale: 0.9 }}
+                    type='submit'
+                    className='bg-blue-600 hover:bg-primary hover:text-white duration-500 px-5 w-full py-2 text-black rounded-lg font-bold'
+                >
+                    Submit
+                </motion.button>
             </form>
         </div>
     );
--- src/components/home/hero/HeroSlider.jsx
+++ src/components/home/hero/HeroSlider.jsx
@@ -7,8 +7,8 @@ const HeroSlider = () => {
 
                 <div className=" text-center h-full w-full bg-black bg-opacity-70 text-white flex items-center">
                     <div className="">
-                        <h1 className='text-7xl font-bold font-catamaran  mb-3'>Amber Chefs</h1>
-                        <p className='text-base font-catamaran my-6'>Amber Chefs is an online platform connecting food enthusiasts with talented chefs worldwide. Explore new flavors and cuisines, improve your cooking skills with recipes, tutorials, and expert advice.</p>
+                        <h1 className='text-7xl font-bold font-catamaran  mb-3'>Zooming Wheels</h1>
+                        <p className='text-base font-catamaran my-6'>Zooming wheels is an online platform connecting food enthusiasts with talented chefs worldwide. Explore new flavors and cuisines, improve your cooking skills with recipes, tutorials, and expert advice.</p>
                         <button className='bg-primary px-11 py-3 rounded-full'>Get Started</button>
                     </div>
                 </div>
