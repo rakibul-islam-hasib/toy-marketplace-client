14510c8e5300d72e83e32e88930485f7c3667eb4
Hasib Hossain <boraborhasib@gmail.com>
2023-05-20 20:00:18 +0600

    Today deal section added

---
+++ src/components/home/deal/TodayDeal.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const TodayDeal = () => {
+    return (
+        <div>
+            <div className="text-center  mx-auto mt-16">
+                <h1 className='text-5xl mb-3 uppercase font-catamaran font-bold'>Today's Deals</h1>
+                <div className="w-[40%] mx-auto">
+                    <p className='my-7 text-sm'>Every week, we bring you a carefully curated selection of the hottest and most popular toys that will ignite your child's imagination and bring joy to their playtime. Our team of toy experts scours the market to handpick the best toys, ensuring that each item is of the highest quality and designed to provide endless hours of fun.</p>
+                </div>
+            </div>
+
+
+        </div>
+    );
+};
+
+export default TodayDeal;
