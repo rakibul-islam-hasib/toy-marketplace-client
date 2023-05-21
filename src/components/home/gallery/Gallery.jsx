5b740eb5fe38dd2952ada87a5731b6cbf7e16095
Hasib Hossain <boraborhasib@gmail.com>
2023-05-21 18:44:30 +0600

    Gallery updated

--- src/components/home/gallery/Gallery.jsx
+++ src/components/home/gallery/Gallery.jsx
@@ -16,7 +16,7 @@ const Gallery = () => {
     return (
         <div className='my-32'>
             <h1 className='text-4xl text-center'>Our Featured Gallery </h1>
-            <div className="w-[60%] mx-auto text-center my-4">
+            <div className="w-[60%] mx-auto text-center mt-4 mb-11">
                 <p className='text-sm'>From sleek sports cars that hug the curves of imaginary tracks to powerful monster trucks that conquer any terrain, our gallery showcases an extensive selection of toy cars that will ignite the thrill of racing in young hearts. Choose from an array of iconic models, including classic muscle cars, rugged off-road vehicles, and futuristic super cars.</p>
             </div>
             <PhotoGallery />
--- src/components/home/gallery/PhotoGallery.jsx
+++ src/components/home/gallery/PhotoGallery.jsx
@@ -5,15 +5,26 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/effect-coverflow";
 import "swiper/css/pagination";
-
+import { LazyLoadImage } from 'react-lazy-load-image-component';
 import "./style.css";
 
+const images = [
+    'https://i.ibb.co/F4hcMk7/pexels-pixabay-163768-min.jpg',
+    'https://i.ibb.co/y4mKH54/pexels-nubia-navarro-nubikini-981588-min.jpg',
+    'https://i.ibb.co/KL92z8B/pexels-bhupendra-singh-3358482.jpg',
+    'https://i.ibb.co/4mf3gcd/pexels-nubia-navarro-nubikini-1522186.jpg',
+    'https://i.ibb.co/51wz2Fn/pexels-the-lazy-artist-gallery-2527939.jpg',
+    'https://i.ibb.co/cLvJXby/pexels-polesie-toys-4491565-min.jpg',
+
+
+]
+
 // import required modules
-import { EffectCoverflow, Pagination ,Autoplay } from "swiper";
+import { EffectCoverflow, Pagination, Autoplay } from "swiper";
 const PhotoGallery = () => {
     return (
         <div className='w-[80%] mx-auto'>
-            <Swiper         effect={"coverflow"}
+            <Swiper effect={"coverflow"}
                 grabCursor={true}
                 centeredSlides={true}
                 slidesPerView={"auto"}
@@ -30,27 +41,14 @@ const PhotoGallery = () => {
                     slideShadows: true,
                 }}
                 pagination={true}
-                modules={[EffectCoverflow, Pagination , Autoplay]}
+                modules={[EffectCoverflow, Pagination, Autoplay]}
                 className="mySwiper"
             >
-                <SwiperSlide className='swiper-slide-img'>
-                    <img className='' src="https://i.ibb.co/F4hcMk7/pexels-pixabay-163768-min.jpg" />
-                </SwiperSlide>
-                <SwiperSlide className='swiper-slide-img'>
-                    <img className='' src="https://i.ibb.co/y4mKH54/pexels-nubia-navarro-nubikini-981588-min.jpg" />
-                </SwiperSlide>
-                <SwiperSlide className='swiper-slide-img'>
-                    <img className='' src="https://i.ibb.co/KL92z8B/pexels-bhupendra-singh-3358482.jpg" />
-                </SwiperSlide>
-                <SwiperSlide className='swiper-slide-img'>
-                    <img className='' src="https://i.ibb.co/4mf3gcd/pexels-nubia-navarro-nubikini-1522186.jpg" />
-                </SwiperSlide>
-                <SwiperSlide className='swiper-slide-img'>
-                    <img className='' src="https://i.ibb.co/51wz2Fn/pexels-the-lazy-artist-gallery-2527939.jpg" />
-                </SwiperSlide>
-                <SwiperSlide className='swiper-slide-img'>
-                    <img className='' src="https://i.ibb.co/cLvJXby/pexels-polesie-toys-4491565-min.jpg" />
-                </SwiperSlide>
+                {
+                    images.map(img => <SwiperSlide className='swiper-slide-img' key={img}>
+                        <img loading='lazy' src={img} alt="" />
+                    </SwiperSlide>)
+                }
             </Swiper>
         </div>
     );
